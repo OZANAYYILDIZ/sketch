f521a17aa65643c4af83cd797ae9c00946cbdc0f
Ozan Ayyıldız <ozanayyildiz92@gmail.com>
2022-11-14 17:48:24 +0300

    delete the old classnamed divs before creating news

--- js/script.js
+++ js/script.js
@@ -1,30 +1,36 @@
 const gridContainer = document.querySelector("#grid-container");
 
+let div = document.createElement("div");
+gridContainer.appendChild(div).setAttribute("class", "grid-row");
+
+
 function createOneRow(number){
-    let div = document.createElement("div");
-    gridContainer.appendChild(div).setAttribute("class", "grid-row");
-    const gridRow = document.querySelector(".grid-row");
-    for(let index = 0; index < number; index++){
-        let div = document.createElement("div");
-        gridRow.appendChild(div).setAttribute("class", "grid-item");
+    let gridRow = document.querySelector(".grid-row");
+    if(number == 64){
+        for(let index = 0; index < number * 16; index++){
+            let div = document.createElement("div");
+            gridRow.appendChild(div).setAttribute("class", "grid-item-s");
+        }
+    } else if(number == 256){
+        for(let index = 0; index < number; index++){
+            let div = document.createElement("div");
+            gridRow.appendChild(div).setAttribute("class", "grid-item-m");
+        }
+    } else if(number == 1024){
+        for(let index = 0; index < number / 16 ; index++){
+            let div = document.createElement("div");
+            gridRow.appendChild(div).setAttribute("class", "grid-item-l");
+        }
     }
 }
-
-
-function createGrids(number){
-    for(let index = 0; index < number; index++){
-        createOneRow(number);
-    }
-}
-
-
 
 const select = document.querySelector("#grid-size");
 
-// ihtiyacım olan seçim değiştiğinde fonksiyonu sıfırdan yapması
-// aşağıdaki function bunu yapmıyor önceki gridin üstüne ekliyor
-
 select.addEventListener("change", function(){
-    const sizeValue = select.value;
-    createGrids(sizeValue);
+    let gridRow = document.querySelector(".grid-row");
+    while(gridRow.firstChild){
+        gridRow.removeChild(gridRow.lastChild)
+    }
+    const sizeValue = select.value ** 2;
+    createOneRow(sizeValue);
 })
