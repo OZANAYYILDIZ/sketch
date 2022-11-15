7895e3c5335c35e0a8c58ff05ec49b900979e184
Ozan Ayyıldız <ozanayyildiz92@gmail.com>
2022-11-15 12:42:47 +0300

    add event listener to the grids and change its style

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const gridContainer = document.querySelector("#grid-container");
 
+//gridContainer.addEventListener("mouseover", () => {
+//    gridContainer.classList.add("painted");
+//})
+
 let div = document.createElement("div");
 gridContainer.appendChild(div).setAttribute("class", "grid-row");
 
@@ -7,20 +11,30 @@ gridContainer.appendChild(div).setAttribute("class", "grid-row");
 function createOneRow(number){
     let gridRow = document.querySelector(".grid-row");
     if(number == 64){
-        for(let index = 0; index < number * 16; index++){
+        for(let index = 0; index < number * 64; index++){
             let div = document.createElement("div");
-            gridRow.appendChild(div).setAttribute("class", "grid-item-s");
+            gridRow.appendChild(div).setAttribute("class", "grid-item-s squares");
         }
     } else if(number == 256){
-        for(let index = 0; index < number; index++){
+        for(let index = 0; index < number * 4; index++){
             let div = document.createElement("div");
-            gridRow.appendChild(div).setAttribute("class", "grid-item-m");
+            gridRow.appendChild(div).setAttribute("class", "grid-item-m squares");
         }
     } else if(number == 1024){
-        for(let index = 0; index < number / 16 ; index++){
+        for(let index = 0; index < number / 4 ; index++){
             let div = document.createElement("div");
-            gridRow.appendChild(div).setAttribute("class", "grid-item-l");
+            gridRow.appendChild(div).setAttribute("class", "grid-item-l squares");
         }
+    }
+}
+
+function styleAdd(){
+    const squares = document.querySelectorAll(".squares");
+
+    for(let index = 0 ; index < squares.length ; index++){
+        squares[index].addEventListener("mouseover", () => {
+            squares[index].classList.add("painted");
+        })
     }
 }
 
@@ -33,4 +47,16 @@ select.addEventListener("change", function(){
     }
     const sizeValue = select.value ** 2;
     createOneRow(sizeValue);
+    styleAdd();
+})
+
+const button = document.querySelector("#clear-btn");
+button.addEventListener("click", function(){
+    let gridRow = document.querySelector(".grid-row");
+    while(gridRow.firstChild){
+        gridRow.removeChild(gridRow.lastChild)
+    }
+    const sizeValue = select.value ** 2;
+    createOneRow(sizeValue);
+    styleAdd();
 })
